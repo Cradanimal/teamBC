6c2d1e74e405e182ac9c6a33e970a782bc711726
Cradanimal <cradwimb@gmail.com>
2018-01-05 16:34:43 -0800

    feat: add querying logic to dynamic array

---
+++ dynamicArray.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const processData(input) => {
+    //Enter your code here
+    input = input.split("\n");
+    const [valueN, valueQ] = input.shift().split(' ');
+    const queries = input.reduce((acc, val) => {
+        acc.push(val.split(' '));
+        return acc;
+    }, []);
+
+    console.log(input)
+}
